3ac329017b1ccd30dc7651b3fe9e9c29e509b0b1
z3r069-lab <55541381+z3r069-lab@users.noreply.github.com>
2023-12-28 19:16:04 +0300

    island commit

--- vite-project/src/models/Island.jsx
+++ vite-project/src/models/Island.jsx
@@ -4,7 +4,7 @@ import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { a } from '@react-spring/three';
 
-import islandScene from '..assets/3d/island.glb';
+import islandScene from '../assets/3d/island.glb';
 
 
 const Island = (props) => {
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -10,6 +10,25 @@ import Island from '../models/Island'
       </div>    */}
 
 const Home = () => {
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0,-6.5,-43];
+    let rotation = [0.1,4.7,0];
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9,0.9,0.9]; 
+    }
+    else{
+      screenScale = [1,1,1];    
+    }
+
+    return [screenScale, screenPosition, rotation];
+  }
+
+
+  const [islandScale, islandPosition, islandRotation]  = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       
@@ -20,8 +39,12 @@ const Home = () => {
         <directionalLight/>
         <ambientLight/>
         <hemisphereLight />
-          
-        <Island/>
+
+        <Island  
+          position={islandPosition}
+          scale={islandScale}
+          rotation={islandRotation}
+        />
       </Suspense>
     </Canvas>
       
