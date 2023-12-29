8dde8420e540bf182d37124b6474aaf2ccde59f2
z3r069-lab <55541381+z3r069-lab@users.noreply.github.com>
2023-12-29 22:30:39 +0300

    Update Contact.jsx

--- vite-project/src/pages/Contact.jsx
+++ vite-project/src/pages/Contact.jsx
@@ -1,8 +1,75 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Contact = () => {
+  const [form, setForm] = useState({name:'', email:'', message:''});
+  const [isLoading, setIsLoading] = useState (false)
+  const handleChange = () => {}
+  const handleFocus = () => {}
+  const handleBlur = () => {}
+  const handleSubmit = () => {}
+  
   return (
-    <div>Contact</div>
+    
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in touch</h1>
+
+        <form className='w-full flex flex-col gap-7 mt-14' 
+          onSubmit={handleSubmit}
+        >
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input type="text"
+            name='name'
+            className='input'
+            placeholder='name'
+            required
+            value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur} />
+          </label>
+
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input type="email"
+            name='name'
+            className='input'
+            placeholder='email@gmail.com'
+            required
+            value={form.email}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur} />
+          </label>
+
+          <label className='text-black-500 font-semibold'>
+            Message
+            <textarea 
+            name='message'
+            rows={4}
+            className='textarea'
+            placeholder='message'
+            required
+            value={form.message}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur} />
+          </label>
+          
+          <button type='submit'
+            className='btn'
+            disabled = {isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading? 'Sending...' : 'Send message'}
+          </button>
+
+        </form>
+      </div>
+    </section>
   )
 }
 
